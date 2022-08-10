726195f7b403a9a427d6c74f6597dd1ceef6d01e
JamesCordova <jcordovas@unsa.edu.pe>
2022-08-09 22:55:19 -0500

    proveedor.service.ts se preparo para obtener info del servidor django y para hacer post a el backend django

--- src/app/services/proveedor.service.ts
+++ src/app/services/proveedor.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProveedorService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  public getProveedores(){
+    return this.httpClient.get('http://127.0.0.1:8000/balones/');
+  }
+
+  public agregarProveedor(nombre : string, ruc : number, direccion : string ){
+    return this.httpClient.post('http://127.0.0.1:8000/balones/', {
+      nombre : nombre,
+      ruc : ruc,
+      direccion : direccion,
+    })
+  }
 }
